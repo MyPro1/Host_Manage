3c21f8f37de4ab3961d4a209dfd86a4f4b6be607
45yujing@sina.com <45yujing@sina.com>
2016-02-24 09:47:34 +0800

    commit

--- app01/tests.py
+++ app01/tests.py
@@ -1,12 +1,5 @@
 #coding:utf-8
 
-files = '/etc/zabbix/zabbix_agentd.conf'
-content = """
-UserParameter=procexist[*],/home/zabbix/monitor/zabbix_procexist.sh $1
-UserParameter=fileexist[*],/home/zabbix/monitor/fileexist.sh $1
-UserParameter=filesize[*],/home/zabbix/monitor/filesize.sh $1
 """
-
-f = open(files,'a')
-f.write(content)
-f.close()
+这里仅仅是一个测试，是否能覆盖之前的test文件
+"""
